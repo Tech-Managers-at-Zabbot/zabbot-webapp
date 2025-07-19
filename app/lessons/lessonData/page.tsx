311fa9e316fe6259d7d3cab99bb90ec210dea640
Akem-Ben <akemini.ndaobong@gmail.com>
2025-07-19 03:00:40 +0100

    Lesson page templagte setup

---
+++ app/lessons/lessonData/page.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+
+
+
+const lessonData = () => {
+  return (
+    <div
+      className="bg-[#fef7d0] min-h-screen w-full"
+      style={{ fontFamily: "Lexend" }}
+    >
+        <header className="bg-[url('/lessons/lesson-top.png')] absolute top-0 w-full bg-contain bg-center min-h-[100px]">
+        </header>
+        <section>
+            
+        </section>
+       <footer className="bg-[url('/lessons/lesson-description-footer.png')] absolute bottom-0 w-full bg-contain bg-center min-h-[100px]">
+       </footer>
+    </div>
+  );
+}
+
+
+export default lessonData;
