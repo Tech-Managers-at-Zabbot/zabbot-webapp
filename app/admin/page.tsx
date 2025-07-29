01346526a3c4eab74621a01aaedb0e855f141077
Akem-Ben <akemini.ndaobong@gmail.com>
2025-07-30 00:01:44 +0100

    Fix: Button alignment

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1251,7 +1251,7 @@ const CreateContentPage = () => {
                   width="200px"
                 >
                   {isLoading || creatCourseLoading ? (
-                    <div className="flex items-center">
+                    <div className="flex items-center justify-center">
                       <div className="animate-spin rounded-full h-4 w-4 border-b-2 border-white mr-2"></div>
                       Creating...
                     </div>
