59c8f4ecabb6b08dbf10f605b4918180b0fa6f92
adexphem <adexphem4real@gmail.com>
2026-06-21 14:42:39 +0100

    jun20-fixes

--- components/dashboard/FlashCard.tsx
+++ components/dashboard/FlashCard.tsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import InAppButton from "../InAppButton";
 import { useRouter } from "next/navigation"
 import { useLoading } from "@/contexts/LoadingProvider";
+import { flashcardsData } from "@/constants/flashcards";
+import { FLASHCARD_STORAGE_KEY } from "@/constants/localstorageKeys";
 
 const FlashCard = () => {
   const router = useRouter()
-  const { setLoading } = useLoading()
+  const { setLoading } = useLoading();
+  const [flashCardIndex, setCurrentFlashCardIndex] = useState(0);
 
   const handleRedirect = () => {
     setLoading(true)
     return router.push("/flashcards")
   }
+
+  useEffect(() => {
+    const saved = localStorage.getItem(FLASHCARD_STORAGE_KEY);
+    if (saved) {
+      const parsed = JSON.parse(saved);
+      setCurrentFlashCardIndex(parsed.currentIndex);
+    }
+  }, []);
+
+  const card = flashcardsData[flashCardIndex];
 
   return (
     <div
@@ -18,7 +31,7 @@ const FlashCard = () => {
       style={{ fontFamily: "Lexend" }}
     >
       <div className="text-[#162B6E] text-[23px] font-[600]">Flash Cards</div>
-      <div className="text-[#000000CC] font-[700] text-[36px]">àlàáfíà</div>
+      <div className="text-[#000000CC] font-[700] text-[36px]">{card.yorubaWord}</div>
       <div className="text-[#666666] font-[400]">
         Mastering alphabet sounds builds your Yorùbá fluency and tone precision.
       </div>
---
+++ constants/localstorageKeys.ts
@@ -0,0 +1,1 @@
+export const FLASHCARD_STORAGE_KEY = "zabbot_flashcards_state";
